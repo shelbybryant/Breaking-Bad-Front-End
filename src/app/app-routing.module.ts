be56c3155e070291784d3934c1cd3290855fbb4d
Sherilyn Michael <sherimichael@gmail.com>
2020-09-18 11:56:37 -0400

    need to update routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
-import { AboutComponent } from './components/about/about.component';
+import { ProfileComponent } from './components/profile/profile.component';
 import { RegisterComponent } from './components/register/register.component';
 
 
@@ -10,15 +10,15 @@ const routes: Routes = [
 
   {
     path: '',
-    component: AboutComponent
+    component: LoginComponent
   },
   {
     path: 'home',
     component: HomeComponent
   },
   {
-    path: 'login',
-    component: LoginComponent
+    path: 'profile',
+    component: ProfileComponent
   },
   {
     path: 'register',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { HomeComponent } from './components/home/home.component';
-import { AboutComponent } from './components/about/about.component';
+import { ProfileComponent } from './components/profile/profile.component';
 import { RegisterComponent } from './components/register/register.component';
 
 import { HttpClientModule } from '@angular/common/http';
@@ -17,10 +17,9 @@ import { HttpClientModule } from '@angular/common/http';
     AppComponent,
     LoginComponent,
     HomeComponent,
-    AboutComponent,
-    RegisterComponent,
+    ProfileComponent,
+    RegisterComponent
    
-    
   ],
   imports: [
     BrowserModule,
