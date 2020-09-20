367f02db8e7642bdc08b348349dc62fcf79a6ef8
Sherilyn Michael <sherimichael@gmail.com>
2020-09-20 14:07:01 -0400

    added AuthenticationService with checkLogin() - hardcoded email and pword to check if works

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,11 @@ import { LoginComponent } from './components/login/login.component';
 import { HomeComponent } from './components/home/home.component';
 import { ProfileComponent } from './components/profile/profile.component';
 import { RegisterComponent } from './components/register/register.component';
+import { UserComponent } from './components/user/user.component';
 import { TriviagameComponent } from './components/triviagame/triviagame.component';
 import { FormsModule } from '@angular/forms';
 
 import { HttpClientModule } from '@angular/common/http';
-import { UserComponent } from './components/user/user.component';
 
 @NgModule({
   declarations: [
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,12 @@
+/*
+* This component takes user login input and passes it to authenticationService
+* mehtod: checkLogin()- This method checks if the user credentials are correct 
+* by calling the previously created AuthenticationService
+*/
+
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthenticationService } from '';
 
 @Component({
   selector: 'app-login',
@@ -8,9 +16,22 @@ import { Component, OnInit } from '@angular/core';
 export class LoginComponent implements OnInit {
 
   error: string;
+  email = 'batman@email.com'
+  password = ''
+  invalidLogin = false
 
-  constructor() { }
+  constructor(private router: Router,
+    private loginservice: AuthenticationService) { }
 
-  ngOnInit(): void {}
+  ngOnInit() {}
+
+  checkLogin() {
+    if (this.loginservice.authenticate(this.email, this.password)
+    ) {
+      this.router.navigate([''])
+      this.invalidLogin = false
+    } else
+      this.invalidLogin = true
+  }
 
 }
